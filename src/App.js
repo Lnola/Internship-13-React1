0f80b52170c3aab8c8e227c316dcacf494c1418d
Lnola <nola.luka@gmail.com>
2019-04-05 21:59:01 +0200

    A few adjustments

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      stateOfResults: [true, true, true]
+      stateOfResults: [true, true, true],
+      color: 'black'
     }
   }
 
@@ -26,16 +27,24 @@ class App extends Component {
     this.setState({ stateOfResults: changedArray })
   }
 
+  resetColorsButton = e=> {
+    this.setState({color: "black"});
+  }
+
+  resetIndexesButton = e=> {
+    this.setState({stateOfResults: [true,true,true]});
+  }
+
   shouldTextBeBold = color => {
     if (this.state.color === color)
       return 'bold';
-    return 'normal';
+    return '';
   }
 
   shouldTextBeNormal = index => {
     if (this.state.stateOfResults[index - 1])
       return 'bold';
-    return 'normal';
+    return '';
   }
 
   render() {
@@ -45,11 +54,13 @@ class App extends Component {
         <p style={{ fontWeight: this.shouldTextBeBold("red") }} onClick={e => this.changeColors('red', e)}>Red</p>
         <p style={{ fontWeight: this.shouldTextBeBold("blue") }} onClick={e => this.changeColors('blue', e)}>Blue</p>
         <p style={{ fontWeight: this.shouldTextBeBold("green") }} onClick={e => this.changeColors('green', e)}>Green</p>
+        <button onClick={e=>this.resetColorsButton(e)}>Reset</button>
 
         <h1>Indexes</h1>
         <p style={{ fontWeight: this.shouldTextBeNormal(1) }} onClick={e => this.changeStateOfResults(1, e)}>1</p>
         <p style={{ fontWeight: this.shouldTextBeNormal(2) }} onClick={e => this.changeStateOfResults(2, e)}>2</p>
         <p style={{ fontWeight: this.shouldTextBeNormal(3) }} onClick={e => this.changeStateOfResults(3, e)}>3</p>
+        <button onClick={e=>this.resetIndexesButton(e)}>Reset</button>
 
         <h1>Result</h1>
         <ColoredText color={this.state.color} label="1" isSelected={this.state.stateOfResults[0]} />
--- src/components/coloredText.js
+++ src/components/coloredText.js
@@ -2,8 +2,14 @@ import React from "react";
 
 const ColoredText = props => {
   if (!props.isSelected)
-    return <p>{props.label}</p>;
-  return <p style={{ color: props.color }}>{props.label}</p>;
+    return (
+      <p style={{ fontWeight: "bold", fontSize: "30px" }}>{props.label}</p>
+    );
+  return (
+    <p style={{ color: props.color, fontWeight: "bold", fontSize: "30px" }}>
+      {props.label}
+    </p>
+  );
 };
 
 export default ColoredText;
